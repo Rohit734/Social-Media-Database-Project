b0e176824257a404029ed73450360d8d2e1327f5
Rohit Pradhan <85367563+Rohit734@users.noreply.github.com>
2023-11-23 18:46:36 +0530

    Update query.sql with new query

--- query.sql
+++ query.sql
@@ -14,4 +14,40 @@ select * from post order by post_id;
 select * from post_tags order by id;
 
 
--- Tushar Singh
+select post.post_id,user_id,users.username,caption,location,photos.photo_url,videos.video_url
+from post join photos on post.photo_id = photos.id 
+join videos on post.video_id = videos.id 
+join users on  post.user_id = users.id order by user_id;
+
+-- find total no of post
+select users.username,count(users.username)
+from post join users on  post.user_id = users.id group by user_id order by count(users.username) desc;
+
+-- find followers
+select username,count(follower_id) as followers from users join follows on users.id = follows.followee_id group by follows.followee_id order by followers desc;
+
+-- total count of hashtag followed by users  
+select users.username,count(hashtag_id) as total_hastag_followed
+from hashtag_follow 
+join users on hashtag_follow.user_id = users.id
+group by users.username order by total_hastag_followed desc;
+
+-- maximum followed hastag
+select hashtag_name, count(hashtag_follow.hashtag_id)
+from hashtags join hashtag_follow on
+hashtags.id = hashtag_follow.hashtag_id group by hashtag_name
+order by count(hashtag_follow.hashtag_id) desc limit 1;
+
+
+
+-- total number of device user login from  
+-- find total no of post per user or max no of post mad by user that makes 
+-- find followers
+-- total count of hashtag followed by users  
+-- maximum followed hastag
+-- who has higest like and in which post 
+-- who made most of comments
+-- which hashtag is mostly used in post 
+-- max bookmark done by user
+-- which post has higest bookmark 
+-- who uploaded max videos and photos with size 
